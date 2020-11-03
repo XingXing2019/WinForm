2f4998015eab5a82a01e779ca23b6aa20d3c455e
Xing <x.xing.unsw@gmail.com>
2020-11-03 21:37:04 +1100

    init

---
+++ MusicPlayer/MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.ltbMusics = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(32, 22);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(104, 37);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(8, 8);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(142, 22);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(104, 37);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(252, 22);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(104, 37);
+            this.btnPrevious.TabIndex = 3;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // ltbMusics
+            // 
+            this.ltbMusics.FormattingEnabled = true;
+            this.ltbMusics.ItemHeight = 16;
+            this.ltbMusics.Location = new System.Drawing.Point(32, 81);
+            this.ltbMusics.Name = "ltbMusics";
+            this.ltbMusics.Size = new System.Drawing.Size(324, 340);
+            this.ltbMusics.TabIndex = 4;
+            this.ltbMusics.DoubleClick += new System.EventHandler(this.ltbMusics_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 450);
+            this.Controls.Add(this.ltbMusics);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.ListBox ltbMusics;
+    }
+}
+
---
+++ MusicPlayer/MusicPlayer/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Media;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<string> musicList;
+        private readonly SoundPlayer sound;
+        public Form1()
+        {
+            InitializeComponent();
+            musicList = new List<string>();
+            sound = new SoundPlayer();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var ofd = new OpenFileDialog
+                {
+                    Title = "Select Musics",
+                    InitialDirectory = @"C:\Users\xxing\OneDrive\Desktop\Musics",
+                    Multiselect = true,
+                };
+                ofd.ShowDialog();
+                var paths = ofd.FileNames;
+                musicList.AddRange(paths);
+                foreach (var path in paths)
+                    ltbMusics.Items.Add(Path.GetFileName(path));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void ltbMusics_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                sound.SoundLocation = musicList[ltbMusics.SelectedIndex];
+                sound.Play();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var index = ltbMusics.SelectedIndex + 1;
+                index %= ltbMusics.Items.Count;
+                ltbMusics.SelectedIndex = index;
+                sound.SoundLocation = musicList[index];
+                sound.Play();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var index = ltbMusics.SelectedIndex - 1;
+                index += index < 0 ? ltbMusics.Items.Count : 0;
+                ltbMusics.SelectedIndex = index;
+                sound.SoundLocation = musicList[index];
+                sound.Play();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
